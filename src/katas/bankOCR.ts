9081d61556c6a6439c1d2e50204923a3440c1be5
Etienne BLANC <etienne.blanc94@gmail.com>
2020-01-09 16:21:29 +0100

    bankOCR progress

--- src/katas/bankOCR.ts
+++ src/katas/bankOCR.ts
@@ -8,11 +8,9 @@ export const getNumberFromNumber3x3 = (numb: Number3x3Lines) => {
   }
 };
 
-export const getNumbers3x3FromLines3x27 = () => {};
-
 export const getLines3x27ArrayFromString = (text: string) => {
   const lines = text.split("\n");
-  const arrayOf3x27Lines = [];
+  const arrayOf3x27Lines: Array<Array<string>> = [];
 
   let tempArray = [];
   let count = 0;
@@ -29,5 +27,55 @@ export const getLines3x27ArrayFromString = (text: string) => {
       count = 0; // Reinit count
     }
   }
+
   return arrayOf3x27Lines;
+};
+
+export const getnumbers3x3FromLines3x27 = (lines3x27: Array<string>) => {
+  const numbers3x3: Array<Number3x3Lines> = [];
+
+  let lines: Array<Array<string>> = [];
+
+  let tempLine: Array<string> = [];
+  let tempString = "";
+
+  // GET EACH 3 CHAR:
+  // Each lines:
+  for (let line in lines3x27) {
+    let count = 0;
+    // Each char:
+    for (let i = 0; i < lines3x27[line].length; i++) {
+      if (count < 3) {
+        tempString = tempString + lines3x27[line][i];
+        count++;
+      }
+      // If blank between numbers
+      if (count === 3) {
+        tempLine.push(tempString);
+
+        // Reinit tempString and count:
+        tempString = "";
+        count = 0;
+      }
+    }
+
+    // Save and reinit:
+    lines.push(tempLine);
+    tempLine = [];
+  }
+
+  // GROUP EACH NUMBER:
+  for (let i = 0; i < lines[0].length; i++) {
+    let tempNumber3x3: Number3x3Lines = { 0: "", 1: "", 2: "" }; //[]
+
+    // Todo: find a best way
+    tempNumber3x3[0] = lines[0][i];
+    tempNumber3x3[1] = lines[1][i];
+    tempNumber3x3[2] = lines[2][i];
+    //tempNumber3x3.push(lines[line][i]);
+
+    numbers3x3.push(tempNumber3x3);
+  }
+  console.log(numbers3x3);
+  return numbers3x3;
 };
--- src/tests/bankOCR.test.ts
+++ src/tests/bankOCR.test.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import {
   getNumberFromNumber3x3,
-  getNumbers3x3FromLines3x27,
+  getnumbers3x3FromLines3x27,
   getLines3x27ArrayFromString
 } from "../katas/bankOCR";
 import { numbers } from "../katas/bankOCR.types";
@@ -13,17 +13,17 @@ const contentUseCase1 = fs.readFileSync(
 
 describe("BankOCR Tests...", () => {
   describe("getNumberFromNumber3x3():", () => {
-    // Check goods args:
+    // Check with goods args:
     for (let i = 0; i < 10; i++) {
-      it("Should be " + i, () => {
+      test(`Number 3x3 (${i}) should be ${i}`, () => {
         expect(getNumberFromNumber3x3(numbers[i].lines)).toBe(i);
       });
     }
 
-    // TODO:Check bads args:
+    // TODO:Check with bads args:
   });
   describe("getLines3x27ArrayFromString()", () => {
-    it("Should be an array", () => {
+    test("Type of output[0] should be an array", () => {
       expect(typeof getLines3x27ArrayFromString(contentUseCase1)).toBe(
         typeof []
       );
@@ -32,14 +32,24 @@ describe("BankOCR Tests...", () => {
       );
     });
 
-    it("Should be a string", () => {
+    test("Type of output[0][O] should be string", () => {
       expect(typeof getLines3x27ArrayFromString(contentUseCase1)[0][0]).toBe(
         typeof ""
       );
     });
+  });
 
-    //describe("getNumbers3x3FromLines3x27()", () => {
-    //const lines = contents.split("\n");
-    //});
+  describe("getNumbers3x3FromLines3x27()", () => {
+    const lines3x27Array = getLines3x27ArrayFromString(contentUseCase1);
+
+    test("Output length of correct input should be 9", () => {
+      expect(getnumbers3x3FromLines3x27(lines3x27Array[0]).length).toBe(9);
+    });
+    // Todo: continue here
+    test("Output length of correct input should be 9", () => {
+      expect(getnumbers3x3FromLines3x27(lines3x27Array[0])[0]).toBe(
+        numbers[1].lines
+      );
+    });
   });
 });
